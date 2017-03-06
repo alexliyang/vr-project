bb6e062e37c7f444c1ea35cb5e8c3356489e957e
Santi Puch Giner <santipuch590@gmail.com>
2017-03-06 20:44:23 +0100

    Fixed train.py script

--- code/train.py
+++ code/train.py
@@ -84,7 +84,7 @@ def main():
     parser.add_argument('-e', '--exp_name', type=str,
                         default=None, help='Name of the experiment')
     parser.add_argument('-s', '--shared_path', type=str,
-                        default='/home/master/data', help='Path to shared data folder'')
+                        default='/home/master/data', help='Path to shared data folder')
     parser.add_argument('-l', '--local_path', type=str,
                         default='/home/master/data', help='Path to local data folder')
 
