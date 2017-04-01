83f44361cc2edb5c6ca94d3a1cc9f88f97195a0b
Belén <luquelopez.belen@gmail.com>
2017-04-01 17:30:23 +0200

    segnet training

--- code/config/camvid_segnet.py
+++ code/config/camvid_segnet.py
@@ -25,9 +25,9 @@ debug_images_test = 50  # N images for testing in debug mode (-1 means all)
 debug_n_epochs = 2  # N of training epochs in debug mode
 
 # Batch sizes
-batch_size_train = 12  # Batch size during training
-batch_size_valid = 12  # Batch size during validation
-batch_size_test = 12  # Batch size during testing
+batch_size_train = 5  # Batch size during training
+batch_size_valid = 5  # Batch size during validation
+batch_size_test = 5  # Batch size during testing
 crop_size_train = None  # Crop size during training (Height, Width) or None
 crop_size_valid = None  # Crop size during validation
 crop_size_test = None  # Crop size during testing
@@ -44,7 +44,7 @@ seed_valid = 1924  # Random seed for the validation shuffle
 seed_test = 1924  # Random seed for the testing shuffle
 
 # Training parameters
-optimizer = 'rmsprop'  # Optimizer
+optimizer = 'adam'  # Optimizer
 learning_rate = 0.0001  # Training learning rate
 weight_decay = 0.  # Weight decay or L2 parameter norm penalty
 n_epochs = 1000  # Number of epochs during training
