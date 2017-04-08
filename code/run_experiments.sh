53afd9e41160566bb13e5fc1ade961d1e4e336f3
Santi Puch <santipuch590@gmail.com>
2017-04-08 12:53:44 +0200

    New experiments for DeepLab and Tiramisu

--- code/run_experiments.sh
+++ code/run_experiments.sh
@@ -4,12 +4,17 @@
 # WARNING: ITS CONTENT IS GOING TO FREQUENTLY CHANGE, AS WE ARE GOING TO CUSTOMIZE IT FOR EACH BATCH
 # OF EXPERIMENTS WE WANT TO EXECUTE.
 
-# Tiramisu
-
-python train.py -c config/camvid_tiramisu_fc103_enhanced.py -e tiramisu_fc103_enhanced
-
-python train.py -c config/camvid_tiramisu_fc103_enhanced_finetune.py -e tiramisu_fc103_enhanced_finetune
-
 # DeepLab
 
-python train.py -c config/camvid_deeplabv2_bilinear_wd -e deeplabv2_bilinear_wd
+python train.py -c config/camvid_deeplabv2_bilinear_wd.py -e deeplabv2_bilinear_wd
+
+
+# Tiramisu
+
+python train.py -c config/camvid_tiramisu_fc67_enhanced.py -e tiramisu_fc67_enhanced
+
+python train.py -c config/camvid_tiramisu_fc67_enhanced_finetune.py -e tiramisu_fc67_enhanced_finetune
+
+python train.py -c config/camvid_tiramisu_fc56_enhanced.py -e tiramisu_fc56_enhanced
+
+python train.py -c config/camvid_tiramisu_fc56_enhanced_finetune.py -e tiramisu_fc56_enhanced_finetune
