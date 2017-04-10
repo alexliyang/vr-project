a0d7a0a26f67d1bb6697176c829330c8ca32b6d9
Santi Puch <santipuch590@gmail.com>
2017-04-11 00:12:05 +0200

    More experiments

--- code/run_experiments.sh
+++ code/run_experiments.sh
@@ -4,17 +4,9 @@
 # WARNING: ITS CONTENT IS GOING TO FREQUENTLY CHANGE, AS WE ARE GOING TO CUSTOMIZE IT FOR EACH BATCH
 # OF EXPERIMENTS WE WANT TO EXECUTE.
 
-# DeepLab
-
-python train.py -c config/camvid_deeplabv2_bilinear_wd.py -e deeplabv2_bilinear_wd
-
-
-# Tiramisu
-
-python train.py -c config/camvid_tiramisu_fc67_enhanced.py -e tiramisu_fc67_enhanced
-
-python train.py -c config/camvid_tiramisu_fc67_enhanced_finetune.py -e tiramisu_fc67_enhanced_finetune
-
-python train.py -c config/camvid_tiramisu_fc56_enhanced.py -e tiramisu_fc56_enhanced
 
 python train.py -c config/camvid_tiramisu_fc56_enhanced_finetune.py -e tiramisu_fc56_enhanced_finetune
+
+python train.py -c config/camvid_deeplabv2_paper.py -e deeplabv2_paper
+
+python train.py -c config/camvid_segnetbasic_scratch_paper.py -e segnetbasic_scratch_paper
